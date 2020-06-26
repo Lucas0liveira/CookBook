8291cf092b445e59ba070444ff73cd323edb388d
Gabriel Borges <gabriel.bgs00@gmail.com>
2020-06-26 14:46:52 -0400

    receita com usuário

--- backend/src/Database/migrations/20200404160403_create_recipes.js
+++ backend/src/Database/migrations/20200404160403_create_recipes.js
@@ -11,9 +11,11 @@ exports.up = function (knex) {
         table.integer('category_id').notNullable()
         table.integer('prepTime').notNullable()
         table.string('prepUnit').notNullable()
+        table.integer('user_id').notNullable()
         table.integer('rating')
 
         table.foreign('category_id').references('id').inTable('categories')
+        table.foreign('user_id').references('id').inTable('users')
     })
 };
 
