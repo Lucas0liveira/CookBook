39c6b4009a8b6ea878744f48b8c782de0677b63b
Leonardo Felipe Oliveira Freitas <leoufms@gmail.com>
2020-06-27 15:03:24 -0300

    Correções na home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -38,7 +38,7 @@ export default function Home() {
 
             <h2 class="title-section">As mais bem avaliadas</h2>
             <Row className="justify-content-md-center">
-                {recipes.map(recipe => ( //resolver segundo acesso ao banco e trocar para indexbyrating
+                {recipeByStar.map(recipe => ( //resolver segundo acesso ao banco e trocar para indexbyrating
                     (
                         <Col xs="auto">
                             <Card style={{ width: '13rem' }}>
@@ -63,7 +63,7 @@ export default function Home() {
 
             <h2 class="title-section">Adicionadas recentemente</h2>
             <Row className="justify-content-md-center">
-                {recipeByStar.map(recipe => ( //resolver segundo acesso ao banco e trocar para indexbyrating
+             {recipes.map(recipe => ( //resolver segundo acesso ao banco e trocar para indexbyrating
                     (
                         <Col xs="auto">
                             <Card style={{ width: '13rem' }}>
